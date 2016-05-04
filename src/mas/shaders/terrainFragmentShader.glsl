d02d72e9d6020cfd6668e04b0f9c3b56bac75605
Jean-Baptiste <theamateis@gmail.com>
2016-05-04 11:10:29 +0200

    Change terrain outline juste for fun

--- src/mas/shaders/terrainFragmentShader.glsl
+++ src/mas/shaders/terrainFragmentShader.glsl
@@ -7,5 +7,5 @@ out vec4 out_Color;
 uniform sampler2D textureSampler;
 
 void main(void) {
-	out_Color = texture(textureSampler, pass_textureCoords);
+	out_Color = texture(textureSampler, pass_textureCoords).aaaa;
 }
